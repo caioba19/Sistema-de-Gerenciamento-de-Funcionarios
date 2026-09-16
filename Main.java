c23f9bb6885708d88080dea0448b5d2896bdabf2
Caio Vinicius Santos da Costa Leite <157418334+caioba19@users.noreply.github.com>
2026-09-16 09:06:41 -0300

    Fix salary output for second employee

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ void main(){
   Gerente Funcionario = new Gerente( "Caioba", 123, 6969, "Aura + EGO", 1010);
 
   System.out.println(p1.getNome() + " Salario Funcionario - " + p1.getSalario());
-  System.out.println(p2.getNome() + " Salario Funcionario - " + p1.getSalario());
+  System.out.println(p2.getNome() + " Salario Funcionario - " + p2.getSalario());
   System.out.println(Funcionario.getNome() + " Departamento do Gerente - " + Funcionario.getDepartamento());
 }
