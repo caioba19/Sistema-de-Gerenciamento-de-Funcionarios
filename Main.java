802337349e227f73b81fc7fd1878720ec71d1104
Caio Vinicius Santos da Costa Leite <157418334+caioba19@users.noreply.github.com>
2026-09-16 08:57:27 -0300

    Implement main method with Funcionario and Gerente

---
+++ Main.java
@@ -0,0 +1,9 @@
+void main(){
+  Funcionario p1 = new Funcionario("Matheus", 234, 7777);
+  Funcionario p2 = new Funcionario("DN", 123, 6767);
+
+  Gerente Funcionario = new Gerente( "Caioba", 123, 6969, "Aura + EGO", 1010);
+
+  System.out.println(p1.getNome() + " - " + p1.getSalario());
+  System.out.println(Funcionario.getNome() + " - " + Funcionario.getDepartamento());
+}
